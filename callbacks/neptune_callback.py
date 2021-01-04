d80787aada5c384d0e1f453c9e4a01f49cd5b7f7
Krzysztof Soja <k.soja@dicella.com>
2021-01-04 01:52:38 +0100

    callback in progress

---
+++ callbacks/neptune_callback.py
@@ -0,0 +1,115 @@
+import os
+import csv
+import gym
+import numpy as np
+import neptune
+
+from typing import Tuple, Union, NoReturn
+from stable_baselines3.common.callbacks import BaseCallback
+from stable_baselines3.common.evaluation import evaluate_policy
+from stable_baselines3.common.vec_env import VecEnv
+
+
+class NeptuneCallback(BaseCallback):
+    def __init__(self,
+                 logs_freq: int,
+                 evaluate_freq: int,
+                 neptune_account_name: str,
+                 project_name: str,
+                 experiment_name: str,
+                 eval_env: Union[gym.Env, VecEnv],
+                 log_dir: str,
+                 verbose: int = 0):
+        super(NeptuneCallback, self).__init__(verbose)
+        self.logs_freq = logs_freq
+        self.evaluate_freq = evaluate_freq
+        self.log_dir = log_dir
+        self.best_mean_reward = -np.inf
+
+        self.neptune_account_name = neptune_account_name
+        self.project_name = project_name
+        self.experiment_name = experiment_name
+
+        self.eval_env = eval_env
+
+    def _init_callback(self) -> None:
+        neptune.init(self.neptune_account_name + '/' + self.project_name)
+        neptune.create_experiment(self.experiment_name)
+
+        if not os.path.exists(self.log_dir):
+            os.mkdir(self.log_dir)
+
+    def _load_logs(self) -> Union[Tuple[float, float, float, float], NoReturn]:
+        last_rewards = []
+        last_ep_lengths = []
+
+        for file_or_dir in os.listdir(self.log_dir):
+            file_or_dir = os.path.join(self.log_dir, file_or_dir)
+            if os.path.isfile(file_or_dir) and file_or_dir[-len('.csv'):] == '.csv':
+                with open(file_or_dir, 'r') as csv_file:
+                    reader = csv.DictReader(csv_file)
+
+                    rows = list(reader)[2:]
+                    if len(rows) == 0:
+                        continue
+
+                    lasts = min(4, len(rows))
+                    rows = rows[-lasts:]
+
+                    for row in rows:
+                        keys = list(row.keys())
+                        last_rewards.append(float(row[keys[0]]))
+                        last_ep_lengths.append(float(row[keys[1]]))
+
+                    if rows >= 100_000: # ToDo: Wypierdalać csv jak jest za duży
+                        pass
+
+        if len(last_rewards) == 0:
+            return
+
+        last_rewards = np.array(last_rewards)
+        last_ep_lengths = np.array(last_ep_lengths)
+        mean_reward, std_reward = np.mean(last_rewards), np.std(last_rewards)
+        mean_ep_lengths, std_ep_lengths = np.mean(last_ep_lengths), np.std(last_ep_lengths)
+        return mean_reward, std_reward, mean_ep_lengths, std_ep_lengths
+
+    def _evaluate(self):
+        episode_rewards, episode_lengths = evaluate_policy(
+            self.model,
+            self.eval_env,
+            n_eval_episodes=5,
+            render=False,
+            deterministic=True,
+            return_episode_rewards=True,
+        )
+
+        mean_reward, std_reward = np.mean(episode_rewards), np.std(episode_rewards)
+        mean_ep_length, std_ep_length = np.mean(episode_lengths), np.std(episode_lengths)
+        return mean_reward, std_reward, mean_ep_length, std_ep_length
+
+    def _on_step(self) -> bool:
+        if self.n_calls % self.logs_freq == 0:
+            logs = self._load_logs()
+            if logs is None:
+                return True
+            else:
+                mean_reward, std_reward, mean_length, std_length = logs
+
+            neptune.log_metric('mean reward from training', mean_reward)
+            neptune.log_metric('std reward from training', std_reward)
+            neptune.log_metric('mean length from training', mean_length)
+            neptune.log_metric('std length from training', std_length)
+
+        if self.n_calls % self.evaluate_freq == 0:
+            mean_reward, std_reward, mean_ep_length, std_ep_length = self._evaluate()
+
+            neptune.log_metric('mean reward from evaluate', mean_reward)
+            neptune.log_metric('std_reward from evaluate', std_reward)
+            neptune.log_metric('mean episode length from evaluate', mean_ep_length)
+            neptune.log_metric('std episode length from evaluate', std_ep_length)
+
+            if mean_reward > self.best_mean_reward:
+                self.best_mean_reward = mean_reward
+                self.model.save(os.path.join(self.log_dir, "best_model"))
+
+        return True
--- main.py
+++ main.py
@@ -1,183 +1,54 @@
 import os
 import gym
-import csv
-import random as rand
-import numpy as np
-import stable_baselines3 as sb3
 
-from typing import Dict, Any, Tuple
-
-from stable_baselines3 import PPO, A2C, SAC, DQN, DDPG, TD3
 from stable_baselines3.common.base_class import BaseAlgorithm
 from stable_baselines3.common.utils import set_random_seed
 from stable_baselines3.common.vec_env import SubprocVecEnv
-from stable_baselines3.common.callbacks import BaseCallback
 from stable_baselines3.common.monitor import Monitor
-from stable_baselines3.common.noise import NormalActionNoise
+
+from getters import *
+from callbacks.neptune_callback import NeptuneCallback
 
 
-class AbstractAlgoGetter:
-
-    multi_processing = False
-
-    @staticmethod
-    def get(environment, verbose=0, **kwargs):
-        raise NotImplementedError
-
-    def __str__(self):
-        raise NotImplementedError
-
-
-class PPOGetter(AbstractAlgoGetter):
-
-    multi_processing = True
-
-    @staticmethod
-    def get(environment, verbose=0, **kwargs):
-        model = PPO(sb3.ppo.MlpPolicy, environment, verbose)
-        return model
-
-    def __str__(self):
-        return 'PPO'
-
-
-class A2CGetter(AbstractAlgoGetter):
-
-    multi_processing = True
-
-    @staticmethod
-    def get(environment, verbose=0, **kwargs):
-        model = A2C(sb3.a2c.MlpPolicy, environment, verbose)
-        return model
-
-    def __str__(self):
-        return 'A2C'
-
-
-class DQNGetter(AbstractAlgoGetter):
-
-    multi_processing = False
-
-    @staticmethod
-    def get(environment, verbose=0, **kwargs):
-        model = DQN(sb3.dqn.MlpPolicy, environment, verbose)
-        return model
-
-    def __str__(self):
-        return 'DQN'
-
-
-class DDPGGetter(AbstractAlgoGetter):
-
-    multi_processing = False
-
-    @staticmethod
-    def get(environment, verbose=0, **kwargs):
-        n_actions = environment.action_space.shape[-1]
-        action_noise = NormalActionNoise(mean=np.zeros(n_actions), sigma=0.1 * np.ones(n_actions))
-        model = DDPG('MlpPolicy', environment, action_noise=action_noise, verbose=verbose)
-        return model
-
-    def __str__(self):
-        return 'DDPG'
-
-
-class SACGetter(AbstractAlgoGetter):
-
-    multi_processing = False
-
-    @staticmethod
-    def get(environment, verbose=0, **kwargs):
-        model = SAC(sb3.sac.MlpPolicy, environment, verbose)
-        return model
-
-    def __str__(self):
-        return 'SAC'
-
-
-class TD3Gettter(AbstractAlgoGetter):
-
-    multi_processing = False
-
-    @staticmethod
-    def get(environment, verbose=0, **kwargs):
-        n_actions = environment.action_space.shape[-1]
-        action_noise = NormalActionNoise(mean=np.zeros(n_actions), sigma=0.1 * np.ones(n_actions))
-        model = TD3('MlpPolicy', environment, action_noise=action_noise, verbose=verbose)
-        return model
-
-    def __str__(self):
-        return 'TD3'
-
-
-class MyCallback(BaseCallback):
-    def __init__(self, check_freq, log_dir, verbose=0):
-        super(MyCallback, self).__init__(verbose)
-        self.check_freq = check_freq
-        self.log_dir = log_dir
-        self.save_path = os.path.join(log_dir, 'best_model')
-        self.best_mean_reward = -np.inf
-
-        if not os.path.exists(self.log_dir):
-            os.mkdir(self.log_dir)
-
-    def _load_logs(self) -> Tuple[float, float]:
-        last_rewards = []
-        for file_or_dir in os.listdir(self.log_dir):
-            if os.path.isfile(file_or_dir) and file_or_dir[-len('.csv'):] == '.csv':
-                with open(file_or_dir, 'r') as csv_file:
-                    reader = csv.DictReader(csv_file)
-
-                    for row in list(reader)[-2:]:
-                        keys = list(row.keys())
-                        try:
-                            last_rewards.append(float(row[keys[0]]))
-                        except ValueError:
-                            pass
-        if len(last_rewards) == 0:
-            return None, None
-
-        last_rewards = np.array(last_rewards)
-        mean_reward, std_reward = np.mean(last_rewards), np.std(last_rewards)
-        return mean_reward, std_reward
-
-    def _on_step(self) -> bool:
-        if self.n_calls % self.check_freq == 0:
-            mean_reward, std_reward = self._load_logs()
-
-            if mean_reward is not None and mean_reward > self.best_mean_reward:
-                self.best_mean_reward = mean_reward
-                self.model.save(self.save_path)
-        return True
-
-
-def make_env(env_id, rank, seed=0):
+def make_env(env_name: str, rank: int, log_dir: str, seed: int = 0):
     """
     Utility function for multiprocessed env.
-    :param env_id: (str) the environment ID
-    :param seed: (int) the inital seed for RNG
-    :param rank: (int) index of the subprocess
+    :param env_name: the environment ID
+    :param rank: index of the subprocess
+    :param log_dir: Path to log directory.
+    :param seed: the inital seed for RNG
     """
+    if not os.path.exists(log_dir):
+        os.mkdir(log_dir)
+
     def _init():
-        env = gym.make(env_id)
+        env = gym.make(env_name)
         env.seed(seed + rank)
-        env = Monitor(env, filename=env_id+'_'+str(rank))
+        env = Monitor(env, filename=os.path.join( log_dir, env_name+'_'+str(rank)))
         return env
     set_random_seed(seed)
     return _init
 
 
 def run(env_name: str, algo_getter: AbstractAlgoGetter, steps: int, workers: int):
+    path_to_log = os.path.join(os.getcwd(), '{}'.format(str(algo_getter)))
+
     if algo_getter.multi_processing:
-        train_env = SubprocVecEnv([make_env(env_name, i) for i in range(workers)], start_method='fork')
+        train_env = SubprocVecEnv([make_env(env_name, i, path_to_log) for i in range(workers)], start_method='fork')
         train_env.reset()
     else:
-        train_env = make_env(env_name, 0)()
+        train_env = make_env(env_name, 0, path_to_log)()
         train_env.reset()
 
+    eval_env = gym.make(env_name)
     model = algo_getter.get(train_env, verbose=1)
-    path_to_log = os.path.join(os.getcwd(), '{}'.format(str(algo_getter)))
-    model.learn(total_timesteps=steps, callback=MyCallback(check_freq=1000, log_dir=path_to_log))
+    model.learn(total_timesteps=steps, callback=NeptuneCallback(logs_freq=100,
+                                                                evaluate_freq=10_000,
+                                                                neptune_account_name="nkrsi",
+                                                                project_name="rl-first-run",
+                                                                experiment_name=model.__class__.__name__,
+                                                                eval_env=eval_env,
+                                                                log_dir=path_to_log))
     return model
 
 
@@ -202,5 +73,7 @@ algo_getters = [PPOGetter(), A2CGetter(), DDPGGetter(), SACGetter(), TD3Gettter(
 for env in envs:
     for algo_getter in algo_getters:
         print("Experiment start in envinronment {} using {}". format(env, str(algo_getter)))
-        run(env, algo_getter, 100, 8)
+        run(env, algo_getter, 100_000_000, 8)
+        break
+    break
 
