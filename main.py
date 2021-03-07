6d5d0ba846a04f427bb2089c46573e042d98e4e3
Krzysztof Soja <k.soja@dicella.com>
2021-03-07 16:10:52 +0100

    dropout done

--- main.py
+++ main.py
@@ -5,18 +5,22 @@ import argparse
 import stable_baselines3 as sb
 import sb3_contrib as sbc
 
-from stable_baselines3.ppo import MlpPolicy as PPOMlpPolicy
-from stable_baselines3.a2c import MlpPolicy as A2CMlpPolicy
-from stable_baselines3.sac import MlpPolicy as SACMlpPolicy
-from stable_baselines3.ddpg import MlpPolicy as DDPGMlpPolicy
-from stable_baselines3.td3 import MlpPolicy as TD3MlpPolicy
-from sb3_contrib.tqc import MlpPolicy as TQCMlpPolicy
 
 from stable_baselines3.common.utils import set_random_seed
 from stable_baselines3.common.vec_env import DummyVecEnv
 
 from callbacks.cyclic_monitor import CyclicMonitor
 from callbacks.neptune_callback import NeptuneCallback
+
+
+from modificated_sb3.ppo import MlpPolicy as PPOMlpPolicy
+from modificated_sb3.a2c import MlpPolicy as A2CMlpPolicy
+from modificated_sb3.sac import MlpPolicy as SACMlpPolicy
+from modificated_sb3.ddpg import MlpPolicy as DDPGMlpPolicy
+from modificated_sb3.td3 import MlpPolicy as TD3MlpPolicy
+from modificated_sb3.tqc import MlpPolicy as TQCMlpPolicy
+
+from common.torch_layers import create_mlp_with_dropout, MlpExtractorWithDropout
 
 
 ENVIRONMENT_NAMES = ['HalfCheetah-v2', 'Ant-v2', 'Walker2d-v2', 'Walker2d-v2', 'Hopper-v2', 'Humanoid-v2']
@@ -55,16 +59,20 @@ if __name__ == '__main__':
     parser.add_argument('--steps', type=int, default=1_000_000)
     parser.add_argument('--workers', type=int, default=1)
     parser.add_argument('--make_video', default=False, action='store_true')
+    parser.add_argument('--dropout', type=float, default=False)
     args = parser.parse_args()
 
     assert args.env in ENVIRONMENT_NAMES, "Environments must be in environment list."
     assert args.algo in sb.__dict__ or args.algo in sbc.__dict__, "Algorithm name must be defined in stable_baselines3."
+    assert args.dropout is False or .0 < args.dropout < 1, "Dropout value must be from zero to one. "
 
     if args.algo not in MULTI_ENV and args.workers != 1:
         print('Chosen algorithm don\'t support multi workers environment.')
         args.workers = 1
 
     print(f'Starting experiment with {args.algo} algorithm in {args.env} with {args.workers} for {args.steps} steps.')
+    if args.dropout is not False:
+        print(f'Algorithm using dropout with value {args.dropout}')
 
     path_to_logs = os.path.join(MAIN_DIR, args.algo + '-' + args.env + '-' + str(time.time()).replace('.', ''))
     if not os.path.exists(path_to_logs):
@@ -77,12 +85,21 @@ if __name__ == '__main__':
         train_env = CyclicMonitor(gym.make(args.env), max_file_size=20, filename=os.path.join(path_to_logs, f'all'))
         eval_env = gym.make(args.env)
 
+    policy_kwargs = dict()
+    if args.dropout is not False:
+        if args.algo in {'A2C', 'PPO'}:
+            policy_kwargs['mlp_extractor_class'] = MlpExtractorWithDropout
+            policy_kwargs['mpl_extractor_kwargs'] = {'dropout_rate': args.dropout}
+        else:
+            policy_kwargs['create_network_function'] = create_mlp_with_dropout
+            policy_kwargs['dropout_rate'] = args.dropout
+
     if args.algo == 'TQC':
         policy_kwargs = dict(n_critics=2, n_quantiles=25)
         model = sbc.TQC(TQCMlpPolicy, train_env, top_quantiles_to_drop_per_net=2, verbose=1,
                         policy_kwargs=policy_kwargs)
     else:
-        model = sb.__dict__[args.algo](POLICY[args.algo], train_env, verbose=1)
+        model = sb.__dict__[args.algo](POLICY[args.algo], train_env, policy_kwargs=policy_kwargs, verbose=1)
     model.learn(total_timesteps=args.steps, callback=NeptuneCallback(model=model,
                                                                      experiment_name=args.env,
                                                                      neptune_account_name='nkrsi',
