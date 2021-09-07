db5b5acf63a579738cdffde595b8a47290a671ad
Krzysztof Soja <k.soja@dicella.com>
2021-09-08 00:35:48 +0200

    fix: tensor type

--- common/gradient_penalty.py
+++ common/gradient_penalty.py
@@ -13,10 +13,10 @@ def gradient_penalty_actor_critic(model: nn.Module, data: th.Tensor,
     gradients_critics = th.autograd.grad(outputs=prediction[1], inputs=data,
                                          grad_outputs=th.ones(prediction[1].size()),
                                          create_graph=True, retain_graph=True)[0]
-
     gradients_actor = th.autograd.grad(outputs=prediction[2], inputs=data,
                                        grad_outputs=th.ones(prediction[2].size()),
                                        create_graph=True, retain_graph=True)[0]
+    gradients_critics, gradients_actor = gradients_critics.to(th.float), gradients_actor.to(th.float)
 
     gradients_critics = gradients_critics.view(data.shape[0], -1)
     gradients_critics_norm = th.sqrt(th.sum(gradients_critics ** 2, dim=1) + 1e-12)
@@ -28,13 +28,14 @@ def gradient_penalty_actor_critic(model: nn.Module, data: th.Tensor,
     return ((gradients_critics_norm - k_for_critic) ** 2).mean(), ((gradients_actor_norm - k_for_actor) ** 2).mean()
 
 
-def gradient_penalty(model: nn.Module, data: th.Tensor, k: float = 1) -> th.Tensor:
+def gradient_penalty(model: nn.Module, data: th.Tensor, k: float = 1,) -> th.Tensor:
     data = th.autograd.Variable(data, requires_grad=True)
     prediction = model(data)
 
     gradients = th.autograd.grad(outputs=prediction[1], inputs=data,
                                  grad_outputs=th.ones(prediction[1].size()),
                                  create_graph=True, retain_graph=True)[0]
+    gradients = gradients.to(th.float)
 
     gradients = gradients.view(data.shape[0], -1)
     gradients_norm = th.sqrt(th.sum(gradients ** 2, dim=1) + 1e-12)
